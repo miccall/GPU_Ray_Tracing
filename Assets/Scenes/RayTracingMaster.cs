241b024276c952181097205767b753e3b63f8c18
miccall <316459829@qq.com>
2019-03-31 19:49:25 +0800

    interact object mesh
    interact object mesh

--- Assets/Scenes/RayTracingMaster.cs
+++ Assets/Scenes/RayTracingMaster.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 // ReSharper disable InconsistentNaming
 // ReSharper disable StringLiteralTypo
@@ -36,6 +37,9 @@ namespace Scenes
         private void OnDisable()
         {
             _sphereBuffer?.Release();
+            _meshObjectBuffer?.Release();
+            _vertexBuffer?.Release();
+            _indexBuffer?.Release();
         }
         
         private void SetShaderParameters()
@@ -46,6 +50,11 @@ namespace Scenes
             RayTracingShader.SetTexture(0, "_SkyboxTexture", skyboxTexture);
             RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
             
+            SetComputeBuffer("_Spheres", _sphereBuffer);
+            SetComputeBuffer("_MeshObjects", _meshObjectBuffer);
+            SetComputeBuffer("_Vertices", _vertexBuffer);
+            SetComputeBuffer("_Indices", _indexBuffer);
+            
             // light dir ;
             var l = DirectionalLight.transform.forward;
             RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity ));
@@ -55,6 +64,7 @@ namespace Scenes
         
         private void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
+            RebuildMeshObjectBuffers();
             SetShaderParameters();
             Render(destination);
         }
@@ -169,6 +179,103 @@ namespace Scenes
             if (spheres.Count <= 0) return;
             _sphereBuffer = new ComputeBuffer(spheres.Count, 56);
             _sphereBuffer.SetData(spheres);
+        }
+
+        private struct MeshObject
+        {
+            public Matrix4x4 localToWorldMatrix;
+            public int indices_offset;
+            public int indices_count;
+        }
+        
+        private static bool _meshObjectsNeedRebuilding = false;
+        private static List<RayTracingObject> _rayTracingObjects = new List<RayTracingObject>();
+        private static List<MeshObject> _meshObjects = new List<MeshObject>();
+        private static List<Vector3> _vertices = new List<Vector3>();
+        private static List<int> _indices = new List<int>();
+        private ComputeBuffer _meshObjectBuffer;
+        private ComputeBuffer _vertexBuffer;
+        private ComputeBuffer _indexBuffer;
+        public static void RegisterObject(RayTracingObject obj)
+        {
+            _rayTracingObjects.Add(obj);
+            _meshObjectsNeedRebuilding = true;
+        }
+
+        public static void UnregisterObject(RayTracingObject obj)
+        {
+            _rayTracingObjects.Remove(obj);
+            _meshObjectsNeedRebuilding = true;
+        }
+        
+        
+        private void RebuildMeshObjectBuffers()
+        {
+            if (!_meshObjectsNeedRebuilding)
+            {
+                return;
+            }
+            _meshObjectsNeedRebuilding = false;
+            _currentSample = 0;
+            // Clear all lists
+            _meshObjects.Clear();
+            _vertices.Clear();
+            _indices.Clear();
+            // Loop over all objects and gather their data
+            foreach (RayTracingObject obj in _rayTracingObjects)
+            {
+                Mesh mesh = obj.GetComponent<MeshFilter>().sharedMesh;
+                // Add vertex data
+                int firstVertex = _vertices.Count;
+                _vertices.AddRange(mesh.vertices);
+                // Add index data - if the vertex buffer wasn't empty before, the
+                // indices need to be offset
+                int firstIndex = _indices.Count;
+                var indices = mesh.GetIndices(0);
+                _indices.AddRange(indices.Select(index => index + firstVertex));
+                // Add the object itself
+                _meshObjects.Add(new MeshObject()
+                {
+                    localToWorldMatrix = obj.transform.localToWorldMatrix,
+                    indices_offset = firstIndex,
+                    indices_count = indices.Length
+                });
+            }
+            CreateComputeBuffer(ref _meshObjectBuffer, _meshObjects, 72);
+            CreateComputeBuffer(ref _vertexBuffer, _vertices, 12);
+            CreateComputeBuffer(ref _indexBuffer, _indices, 4);
+        }
+        private static void CreateComputeBuffer<T>(ref ComputeBuffer buffer, List<T> data, int stride)
+            where T : struct
+        {
+            // Do we already have a compute buffer?
+            if (buffer != null)
+            {
+                // If no data or buffer doesn't match the given criteria, release it
+                if (data.Count == 0 || buffer.count != data.Count || buffer.stride != stride)
+                {
+                    buffer.Release();
+                    buffer = null;
+                }
+            }
+            if (data.Count != 0)
+            {
+                // If the buffer has been released or wasn't there to
+                // begin with, create it
+                if (buffer == null)
+                {
+                    buffer = new ComputeBuffer(data.Count, stride);
+                }
+                // Set data on the buffer
+                buffer.SetData(data);
+            }
+        }
+        private void SetComputeBuffer(string name, ComputeBuffer buffer)
+        {
+            if (buffer != null)
+            {
+                RayTracingShader.SetBuffer(0, name, buffer);
+            }
         }
     }
 }
---
+++ Assets/Scenes/RayTracingObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Scenes;
+using UnityEngine;
+// ReSharper disable CheckNamespace
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class RayTracingObject : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        RayTracingMaster.RegisterObject(this);
+    }
+    private void OnDisable()
+    {
+        RayTracingMaster.UnregisterObject(this);
+    }
+}
